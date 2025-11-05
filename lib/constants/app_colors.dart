979ebd9c15caedeb9c0de8469b1dea4f9bb39ac9
Azmir Khan <azmir.azamkhan@gmail.com>
2025-11-05 12:41:28 +0600

    Splash Screen Implemented

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+class AppColors{
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color black50Percent = Color(0x80000000);
+  static const Color black30Percent = Color(0x4D000000);
+  static const Color orange = Color(0xFFFF9800);
+  static const Color grey86 = Color(0xF868889);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       body: Center(
         child: Text("Hello"),
